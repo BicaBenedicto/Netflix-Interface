76d4a4bf7ec34b3c8afdb471302392183d4ab931
Gabriel <gabrielpbenedicto@gmail.com>
2021-12-28 11:35:24 -0300

    Ajustado mobile version

--- script.js
+++ script.js
@@ -2,30 +2,20 @@ const requestAPI = async (search, type='series', page=1) => {
   const movie = (search.includes(' ') ? search.replace(' ', '%20') : search);
   const response = await fetch(`https://www.omdbapi.com/?apikey=312712c4&s=${movie}&type=${type}&page=${page}`)
   const { Search }= await response.json();
-  console.log(Search);
   return Search;
 }
 
 const DestaqueRender = async (id, search, type = '', index = 1) => {
   const section = document.getElementById(id);
   const movie = await requestAPI(search, type, index);
-  const block = document.createElement('div');
-  block.style.display = 'flex';
-  block.className = `d-flex flex-row flex-nowrap w-100 cards-block-${index} gallery-cell`;
-  section.appendChild(block);
-  return movie.forEach(({ Poster, Title }) => {
+  movie.forEach(({ Poster, Title }, i) => {
   const card = document.createElement('div');
   const img = document.createElement('img');
   img.src = Poster;
   img.alt = Title;
-  img.className = 'd-block w-100 h-100';
+  img.className = 'd-block card-list-item'
 
-  card.appendChild(img);
-
-  card.style.margin = '10px';
-  card.className = 'card-item';
-
-  block.appendChild(card);
+  section.appendChild(img);
   });
 }
 
